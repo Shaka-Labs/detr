52492aa38aa802016bbb8cba5df29de91f603b3d
Seif (MAcbook M3) <vateseif@gmail.com>
2024-04-11 16:03:03 +0200

    change setup

--- setup.py
+++ setup.py
@@ -3,8 +3,15 @@ from setuptools import find_packages
 
 setup(
     name='detr',
-    version='0.0.0',
+    description="DEtection TRansformers (DETR) with PyTorch",
+    version='0.0.1',
     packages=find_packages(),
     license='MIT License',
+    url="https://github.com/Shaka-Labs/detr",
     long_description=open('README.md').read(),
+    install_requires=[],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
